eb39e17bac9e834610f14bb7f0ecccad7665e35e
Max You <max.you15@gmail.com>
2014-03-13 00:22:21 -0700

    fix csv parsing bug where escaped commas were not accounted for

--- data/csv_to_json_converter.js
+++ data/csv_to_json_converter.js
@@ -1,26 +1,37 @@
 
+var async = require('async');
 var fs = require('fs');
 
 var output = [];
 var startYear = 1960;
 var endYear = 2011;
 
-fs.readFile('./world-pop.csv', function(err, data) {
-  var countriesData = data.toString().split('\n').slice(0, -1);
-
-  for (var i = 1; i < countriesData.length; i++) {
-    var countryData = countriesData[i];
-    output.push(countryDataToJSON(countryData));
-  }
-
-  output = JSON.stringify(output, null, 2);
-  fs.writeFile('./world-pop.json', output, function() {
-    console.log('Success! JSON written to ./world-pop.json');
+function deleteFile(callback) {
+  fs.unlink('./world-pop.json', function (err) {
+    console.log('successfully deleted file');
+    return callback(null);
   });
-});
+}
+
+function processFile(callback) {
+  fs.readFile('./world-pop.csv', function(err, data) {
+    var countriesData = data.toString().split('\n').slice(0, -1);
+
+    for (var i = 1; i < countriesData.length; i++) {
+      var countryData = countriesData[i];
+      output.push(countryDataToJSON(countryData));
+    }
+
+    output = JSON.stringify(output, null, 2);
+    fs.writeFile('./world-pop.json', output, function() {
+      console.log('Success! JSON written to ./world-pop.json');
+      return callback(null);
+    });
+  });
+}
 
 function countryDataToJSON(countryData) {
-  var countryDataColumns = countryData.split(',');
+  var countryDataColumns = CSVToArray(countryData)[0];
   var countryPopByYear = [];
 
   for (var j = startYear; j <= endYear; j++) {
@@ -38,4 +49,94 @@ function countryDataToJSON(countryData) {
     code: countryDataColumns[1],
     populationByYear: countryPopByYear
   };
+}
+
+async.series([deleteFile, processFile]);
+
+// THE CODE BELOW IS THIRD PARTY CSV PARSING CODE
+// credit: http://stackoverflow.com/questions/1293147/javascript-code-to-parse-csv-data
+
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray( strData, strDelimiter ){
+  // Check to see if the delimiter is defined. If not,
+  // then default to comma.
+  strDelimiter = (strDelimiter || ",");
+
+  // Create a regular expression to parse the CSV values.
+  var objPattern = new RegExp(
+    (
+      // Delimiters.
+      "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+        // Quoted fields.
+        "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+        // Standard fields.
+        "([^\"\\" + strDelimiter + "\\r\\n]*))"
+      ),
+    "gi"
+  );
+
+
+  // Create an array to hold our data. Give the array
+  // a default empty first row.
+  var arrData = [[]];
+
+  // Create an array to hold our individual pattern
+  // matching groups.
+  var arrMatches = null;
+
+
+  // Keep looping over the regular expression matches
+  // until we can no longer find a match.
+  while (arrMatches = objPattern.exec( strData )){
+
+    // Get the delimiter that was found.
+    var strMatchedDelimiter = arrMatches[ 1 ];
+
+    // Check to see if the given delimiter has a length
+    // (is not the start of string) and if it matches
+    // field delimiter. If id does not, then we know
+    // that this delimiter is a row delimiter.
+    if (
+      strMatchedDelimiter.length &&
+        (strMatchedDelimiter != strDelimiter)
+      ){
+
+      // Since we have reached a new row of data,
+      // add an empty row to our data array.
+      arrData.push( [] );
+
+    }
+
+
+    // Now that we have our delimiter out of the way,
+    // let's check to see which kind of value we
+    // captured (quoted or unquoted).
+    if (arrMatches[ 2 ]){
+
+      // We found a quoted value. When we capture
+      // this value, unescape any double quotes.
+      var strMatchedValue = arrMatches[ 2 ].replace(
+        new RegExp( "\"\"", "g" ),
+        "\""
+      );
+
+    } else {
+
+      // We found a non-quoted value.
+      var strMatchedValue = arrMatches[ 3 ];
+
+    }
+
+
+    // Now that we have our value string, let's add
+    // it to the data array.
+    arrData[ arrData.length - 1 ].push( strMatchedValue );
+  }
+
+  // Return the parsed data.
+  return( arrData );
 }

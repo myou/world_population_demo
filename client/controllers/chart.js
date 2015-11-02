68147ec57e21c9e6651bd4afb86160e0147e1607
Max You <max.you15@gmail.com>
2015-11-02 07:02:07 -0800

    refactor chart controller

--- client/controllers/chart.js
+++ client/controllers/chart.js
@@ -1,41 +1,49 @@
 
 worldPop.controller('ChartCtrl', function($scope, wpData, wpState) {
-  var populationList = [];
-  var processedData = {
-    populationByYear: populationList
-  };
-
   // hack, could not get data bind working properly
   // if doing this for work, would either properly study d3.js data binding
   // or simply use an eventemitter design
   $scope.counter = 0;
-
   $scope.state = wpState;
+  $scope.data = {
+    name: '',
+    code: '',
+    populationByYear: []
+  };;
 
-  $scope.data = processedData;
+  $scope.$watchCollection('state.selectedCountries', calculateDisplayData);
 
-  $scope.$watchCollection('state.selectedCountries', calculateData);
+  function calculateDisplayData() {
+    $scope.counter++; // the hack mentioned above
 
-  function calculateData() {
-    $scope.counter++;
+    $scope.data.name = 'none';
+    $scope.data.code = '';
+    $scope.data.populationByYear.splice(0, $scope.data.populationByYear.length);
 
-    processedData.name = 'none';
-    processedData.code = '';
-    populationList.splice(0, populationList.length);
-
-    var filtered = wpData.data.filter(function(country) {
+    var selected = wpData.data.filter(function(country) {
       return wpState.selectedCountries.indexOf(country.name) !== -1;
     });
 
-    if (filtered.length === 0) return;
+    if (selected.length === 0) return;
 
-    var nameList, name;
-    if (filtered.length === 2) {
-      name = filtered.map(function(country) {
+    $scope.data = {
+      populationByYear: calculateCombinedPopulationList(selected),
+      name: constructSelectedCountryNames(selected),
+      code: selected.map(function(country) {
+        return country.code;
+      }).join(', ')
+    };
+  }
+
+  function constructSelectedCountryNames(selected) {
+    var name;
+
+    if (selected.length === 2) {
+      name = selected.map(function(country) {
         return country.name;
       }).join(' and ');
     } else {
-      nameList = filtered.map(function(country) {
+      var nameList = selected.map(function(country) {
         return country.name;
       });
       name = nameList[nameList.length - 1];
@@ -43,15 +51,17 @@ worldPop.controller('ChartCtrl', function($scope, wpData, wpState) {
       name = nameList.join(', ');
     }
 
-    var code = filtered.map(function(country) {
-      return country.code;
-    }).join(', ');
+    return name;
+  }
 
-    for (var i = 0; i < filtered[0]['populationByYear'].length; i++) {
-      var year = filtered[0]['populationByYear'][i].year;
+  function calculateCombinedPopulationList(selected) {
+    var populationList = [];
+
+    for (var i = 0; i < selected[0]['populationByYear'].length; i++) {
+      var year = selected[0]['populationByYear'][i].year;
       var population = 0;
 
-      filtered.forEach(function(country) {
+      selected.forEach(function(country) {
         population += country.populationByYear[i].population || 0;
       });
 
@@ -61,7 +71,6 @@ worldPop.controller('ChartCtrl', function($scope, wpData, wpState) {
       };
     }
 
-    processedData.name = name;
-    processedData.code = code;
+    return populationList;
   }
 });
